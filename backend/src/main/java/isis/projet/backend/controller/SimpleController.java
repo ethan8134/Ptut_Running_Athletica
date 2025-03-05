ce545d15f259d1a7e812f32083360dbea370d36d
ethan8134 <ethan.cabanes@etud.univ-jfc.fr>
2025-03-05 17:20:18 +0100

    Modèle de base disponible, on part de ça

--- backend/src/main/java/isis/projet/backend/controller/SimpleController.java
+++ backend/src/main/java/isis/projet/backend/controller/SimpleController.java
@@ -1,6 +1,5 @@
 package isis.projet.backend.controller;
 
-import isis.projet.backend.service.CountryService;
 import org.springframework.web.bind.annotation.*;
 import lombok.extern.slf4j.Slf4j;
 import java.util.Map;
